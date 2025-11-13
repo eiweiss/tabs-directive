060fcc287f575c97bba05e86936094a2e3caabc1
CWOIDA <2544086+eiweiss@users.noreply.github.com>
2025-11-13 13:52:35 +0100

    Refactor license and comments in swipe-navigation directive
    Updated license information and improved comments for clarity.

--- src/directives/swipe-navigation.directive.ts
+++ src/directives/swipe-navigation.directive.ts
@@ -1,9 +1,30 @@
 /**
  * @license
+ * MIT License
+ * 
  * Copyright (c) 2025 CWOIDA
- *
- * This file is part of CNGX (Composable Angular Extension).
- * Licensed under the MIT License. See LICENSE file in the project root.
+ * 
+ * This directive is part of CNGX (Composable Angular Extension), an open-source
+ * framework for Angular extensions.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
  */
 
 import {
@@ -407,3 +428,4 @@ export class SwipeNavigationDirective implements AfterViewInit, OnDestroy {
     return this.tabHeaderElement === target || this.tabHeaderElement.contains(target);
   }
 }
+
